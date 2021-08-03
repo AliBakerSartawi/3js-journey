e74facc8dcd50dac2259bd1ecf99e20ecb7375bb
AliBakerSartawi <alisartawi@live.com>
2021-08-03 23:20:36 +0300

    groups are awesome

--- src/lessons/03/BasicScene.jsx
+++ src/lessons/03/BasicScene.jsx
@@ -1,40 +1,40 @@
 import React, { useEffect, useRef } from 'react';
 import { Canvas, useThree, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import * as THREE from 'three'
+import * as THREE from 'three';
 
-const lookAtBox = new THREE.Vector3()
+const lookAtRef = new THREE.Vector3();
 
 // the camera must be used in a component inside the Canvas component
-function CameraLog({ box }) {
+function Camera({ box, group }) {
   const { camera } = useThree();
   // console.log(camera);
   // console.log(box.current?.position.distanceTo(camera.position));
 
   ///// lookAt() method
   // the box is moving and camera is following
-  // useFrame((state, delta) => {
-  //   if (box.current.position.x > 3) {
-  //     box.current.position.x = -3
-  //   } else {
-  //     box.current.position.x += delta * 1
-  //   }
-  // })
+  useFrame((state, delta) => {
+    if (group.current.position.x > 3) {
+      group.current.position.x = -3;
+    } else {
+      group.current.position.x += delta * 1;
+    }
+  });
 
   // useFrame helps in accessing the camera
-  useFrame(state => {
-    lookAtBox.x = box.current.position.x
-    state.camera.lookAt(lookAtBox)
-  })
+  useFrame((state) => {
+    // lookAtRef.x = group.current.position.x;
+    state.camera.lookAt(group.current.position);
+  });
   //// using lookAt() outside useFrame() didn't work for me
   //// lookAt() works on any object, it makes its -z face look at another object
 
   return null;
 }
 
-
 function BasicScene() {
   const box = useRef();
+  const group = useRef();
 
   function log() {
     // console.log(box.current.position);
@@ -65,25 +65,38 @@ function BasicScene() {
     <Canvas camera={{ fov: 75, position: [3, 1, 5] }}>
       <axesHelper args={[10]} />
       <OrbitControls />
-      <CameraLog box={box} />
-      <mesh
-        ref={box}
-        onClick={log}
-        // Math.PI is equal to 180 degrees rotation (half rotation)
-        // quarter rotation => Math.PI / 2 OR Math.PI * 0.5
-        // read notes at bottom of page for advanced rotation
-        rotation={[Math.PI * 0.25, Math.PI * 0.25, 0, 'YXZ']}
+      <Camera box={box} group={group} />
+      <group
+        // changes properties of what's inside the group combined
+        ref={group}
         position={[1, 1, 1]}
-        // scale prop overrides the geometry args
-        scale={[0.5, 0.5, 0.5]}>
-        <axesHelper args={[10]} />
-        <boxGeometry
-          // if args are not specified, defaults are [1, 1, 1]
-          args={[1, 1, 1]}
-        />
-        {/* better way to declare colors is like this "0xff0000" */}
-        <meshBasicMaterial color="crimson" />
-      </mesh>
+        scale={[0.7, 0.7, 0.7]}
+        rotation={[0, 0, 0]}>
+        <axesHelper args={[5]} />
+        <mesh
+          ref={box}
+          onClick={log}
+          // Math.PI is equal to 180 degrees rotation (half rotation)
+          // quarter rotation => Math.PI / 2 OR Math.PI * 0.5
+          // read notes at bottom of page for advanced rotation
+          rotation={[Math.PI * 0.25, Math.PI * 0.25, 0, 'YXZ']}
+          position={[1, 1, 1]}
+          // scale prop overrides the geometry args
+          scale={[0.5, 0.5, 0.5]}>
+          <axesHelper args={[3]} />
+          <boxGeometry
+            // if args are not specified, defaults are [1, 1, 1]
+            args={[1, 1, 1]}
+          />
+          {/* better way to declare colors is like this "0xff0000" */}
+          <meshBasicMaterial color="crimson" />
+        </mesh>
+        <mesh position={[0, 2, 2]} scale={[0.5, 0.5, 0.5]}>
+          <axesHelper args={[3]} />
+          <boxGeometry args={[1, 1, 1]} />
+          <meshBasicMaterial color="lime" />
+        </mesh>
+      </group>
     </Canvas>
   );
 }
