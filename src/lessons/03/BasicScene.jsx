0587f4ae5d252c8659040b7d5aaf958ef5df6c7c
AliBakerSartawi <alisartawi@live.com>
2021-08-03 22:04:06 +0300

    scale

--- src/lessons/03/BasicScene.jsx
+++ src/lessons/03/BasicScene.jsx
@@ -3,42 +3,53 @@ import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
 // the camera must be used in a component inside the Canvas component
-function CameraLog({box}) {
+function CameraLog({ box }) {
   const { camera } = useThree();
   // console.log(camera);
-  console.log(box.current?.position.distanceTo(camera.position))
-  return null
+  console.log(box.current?.position.distanceTo(camera.position));
+  return null;
 }
 
 function BasicScene() {
   const box = useRef();
-  
 
   function log() {
     console.log(box.current.position);
     // length() is the distance between the center of the scene and the object
     console.log(box.current.position.length());
-    
+
     // position.distanceTo(arg) => arg is a Vector3
     // such as position.distanceTo(camera.position)
     // such as position.distanceTo(new THREE.Vector3(1, 0, 3))
-    
+
     // will reduce the length() (distance from center) to 1
-    box.current.position.normalize()
+    box.current.position.normalize();
     console.log(box.current.position.length());
 
     // set changes x, y, z coords
     // box.current.position.set(2, 2, 2)
-    // console.log(box.current.position.length());
+    // box.current.scale.set(2, 2, 2)
+    /// OR target a specific axis => box.current.scale.x
   }
 
   return (
     // default camera fov is 75 approximately
-    <Canvas camera={{ fov: 75, position: [3, 1, 3] }}>
+    <Canvas camera={{ fov: 75, position: [3, 1, 5] }}>
+      <axesHelper args={[10]} />
       <OrbitControls />
       <CameraLog box={box} />
-      <mesh ref={box} onClick={log} position={[0.5, 0.5, 0.5]}>
-        <boxGeometry args={[1, 1, 1]} />
+      <mesh
+        ref={box}
+        onClick={log}
+        rotation={[1, 1, 1]}
+        position={[1, 1, 1]}
+        // scale prop overrides the geometry args
+        scale={[0.5, 0.5, 0.5]}>
+        <axesHelper args={[10]} />
+        <boxGeometry
+          // if args are not specified, defaults are [1, 1, 1]
+          args={[1, 1, 1]}
+        />
         {/* better way to declare colors is like this "0xff0000" */}
         <meshBasicMaterial color="crimson" />
       </mesh>
