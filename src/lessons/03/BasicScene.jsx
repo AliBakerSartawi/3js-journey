c2bbbb25e50816c239709e561767b4554f3d438e
AliBakerSartawi <alisartawi@live.com>
2021-08-03 18:50:21 +0300

    camera position

--- src/lessons/03/BasicScene.jsx
+++ src/lessons/03/BasicScene.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 function BasicScene() {
   return (
-    <Canvas>
-      <OrbitControls enablePan={false} />
+    // default camera fov is 75 approximately
+    <Canvas camera={{fov: 75, position: [3, 1, 3]}}>
+      <OrbitControls />
       <mesh>
         <boxGeometry args={[1, 1, 1]} />
+        {/* better way to declare colors is like this "0xff0000" */}
         <meshBasicMaterial color="crimson" />
       </mesh>
     </Canvas>
-  )
+  );
 }
 
-export default BasicScene
+export default BasicScene;
