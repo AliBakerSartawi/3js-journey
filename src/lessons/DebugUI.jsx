46d5021867566eec22b1cf637097f38e97298828
AliBakerSartawi <alisartawi@live.com>
2021-08-05 05:01:22 +0300

    dat.gui tips

--- src/lessons/DebugUI.jsx
+++ src/lessons/DebugUI.jsx
@@ -8,40 +8,28 @@ import * as dat from 'dat.gui';
 /**
  * Debug
  */
-const gui = new dat.GUI();
-
-const parameters = {
-
-  // color: 'crimson', // this naming convention will cause an error
-  color: 0xff0000, // without quotations
-  colorAsWell: '#ff0000', // without quotations
-}
-
-/**
- * Geometries
- */
-const positionsArray = new Float32Array([
-  // first vertex
-  0, 0, 0,
-  // second vertex
-  0, 1, 0,
-  // third vertex
-  1, 0, 0
-]);
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-
-const count = 500; // number of triangle we want
-// count * 3 (number of vertices) * 3 (x, y, z)
-const positionsArray2 = new Float32Array(count * 3 * 3);
-for (let i = 0; i < positionsArray2.length; i++) {
-  positionsArray2[i] = (Math.random() - 0.5) * 2;
-}
-const positionsAttribute2 = new THREE.BufferAttribute(positionsArray2, 3);
+const gui = new dat.GUI({closed: true, width: 400});
+// gui.hide() // hide/unhide by pressing h
+// https://jsfiddle.net/ikatyang/182ztwao/ // for more examples
 
 /**
  * Debug Component
  */
 function guiAdd(box, material) {
+  // everything must be inside an object in order to be added to dat.gui
+  const parameters = {
+    // color: 'crimson', // this naming convention will cause an error
+    color: 0xff0000, // without quotations
+    colorAsWell: '#ff0000', // without quotations
+    spin: () => {
+      gsap.to(box.current.rotation, {
+        duration: 1,
+        y: box.current.rotation.y + Math.PI * 2,
+        ease: 'power2.out'
+      })
+    }
+  };
+
   // console.log(box)
   gui.add(
     // object name
@@ -57,20 +45,25 @@ function guiAdd(box, material) {
   );
   gui.add(box.current.position, 'y', -3, 3, 0.01);
   // or a cleaner way ⬇️
-  gui.add(box.current.position, 'z').min(-3).max(3).step(0.01).name('boxPositionZ');
+  gui
+    .add(box.current.position, 'z')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('boxPositionZ');
 
-  gui.add(box.current, 'visible')
-    .name('boxVisibility')
+  gui.add(box.current, 'visible').name('boxVisibility');
 
-  gui.add(box.current.material, 'wireframe')
-    .name('boxWireframe')
+  gui.add(box.current.material, 'wireframe').name('boxWireframe');
 
   // the color property is an instance of the Color class in three
   // ... so we cannot tweak it directly, we need a trick
-  gui.addColor(parameters, 'color')
+  gui.addColor(parameters, 'color');
   gui.addColor(parameters, 'colorAsWell').onChange(() => {
-    box.current.material.color.set(parameters.colorAsWell)
-  })
+    box.current.material.color.set(parameters.colorAsWell);
+  });
+
+  gui.add(parameters, 'spin');
 }
 
 /**
@@ -79,13 +72,13 @@ function guiAdd(box, material) {
 function DebugUI() {
   const box = useRef();
 
-  // or we can access it through box.current.material 
+  // or we can access it through box.current.material
   const material = useRef();
 
   useEffect(() => {
     setTimeout(() => {
       guiAdd(box, material);
-    }, 1000);
+    }, 100);
   }, []);
 
   return (
@@ -117,15 +110,6 @@ function DebugUI() {
           <meshBasicMaterial wireframe color="hotpink" />
         </mesh>
 
-        <mesh position={[0, 1, 1.25]}>
-          <bufferGeometry attributes={{ position: positionsAttribute }} />
-          <meshBasicMaterial wireframe color="aqua" />
-        </mesh>
-
-        <mesh position={[0, 1, 2.5]}>
-          <bufferGeometry attributes={{ position: positionsAttribute2 }} />
-          <meshBasicMaterial wireframe color="goldenrod" />
-        </mesh>
       </Canvas>
     </div>
   );
