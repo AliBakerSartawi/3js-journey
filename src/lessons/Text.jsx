81fc48ad02de8130169debe63f72ff8473535965
AliBakerSartawi <alisartawi@live.com>
2021-08-07 23:34:04 +0300

    magic centering

--- src/lessons/Text.jsx
+++ src/lessons/Text.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useHelper } from '@react-three/drei';
 import * as THREE from 'three';
 import gsap from 'gsap';
 
@@ -12,25 +12,33 @@ import gsap from 'gsap';
 
 /**
  * font imports and notes
- * 
+ *
  * convert font to typeface => https://gero3.github.io/facetype.js/
  * or get fonts from /three/examples/fonts
  */
-import typefaceFontFromThree from 'three/examples/fonts/helvetiker_regular.typeface.json'
+import typefaceFontFromThree from 'three/examples/fonts/helvetiker_regular.typeface.json';
 // or load it from src, notice how License file is in same directory as font
-import typefaceFont from '../fonts/helvetiker_regular.typeface.json'
+import typefaceFont from '../fonts/helvetiker_regular.typeface.json';
 
-const font = new THREE.FontLoader().parse(typefaceFont)
+const font = new THREE.FontLoader().parse(typefaceFont);
+const textString = `';hello!?<>/*&%$#@!'`;
 const textOptions = {
   font,
   size: 1,
-  height: .25
-}
+  height: 0.25,
+  // default is 12, 4 is good for small text, 6 || 8 is ideal for performance & quality
+  curveSegments: 8
+};
 
 /**
  * Camera component
  */
 function Motion({ textMesh }) {
+  // if not inside a useEffect, will give ref undefined error
+  useEffect(() => {
+    // easy centering solution instead of computing bounding box and manually translating to -50%'s
+    textMesh.current.geometry.center();
+  }, [textMesh]);
   // useFrame(({clock: {elapsedTime}}) => {
   //   textMesh.current.rotation.x = elapsedTime * 0.1
   //   textMesh.current.rotation.y = elapsedTime * 0.1
@@ -56,8 +64,8 @@ function Lights() {
  * Main Component
  */
 function Text() {
-  const textMesh = useRef()
-  
+  const textMesh = useRef();
+
   return (
     <div style={{ height: '100vh', backgroundColor: 'rgb(26, 26, 26)' }}>
       <Canvas
@@ -75,19 +83,21 @@ function Text() {
         <Motion textMesh={textMesh} />
         <Lights />
 
-      {/* MESHES */}
-      
-      <mesh ref={textMesh}>
-        <textBufferGeometry args={['hello!', textOptions]} />
-        <meshStandardMaterial color={'crimson'} />
-      </mesh>
-      
-      
-      {/* <mesh>
+        {/* MESHES */}
+
+        <mesh ref={textMesh} position={[0, 0.5, 0]}>
+          <textBufferGeometry
+            // parameters is useful for remembering textOptions, but they must be provided in args as below
+            // parameters
+            args={[textString, textOptions]}
+          />
+          <meshStandardMaterial color={'crimson'} />
+        </mesh>
+
+        {/* <mesh>
         <boxBufferGeometry args={[1, 1, 1]} />
         <meshBasicMaterial color={'whitesmoke'} />
       </mesh> */}
-
       </Canvas>
     </div>
   );
