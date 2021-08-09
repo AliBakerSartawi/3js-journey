339bcfd3031dea45560b36e25dc56a440f718d3b
AliBakerSartawi <alisartawi@live.com>
2021-08-09 23:42:05 +0300

    clean custom geo function

--- src/lessons/Particles.jsx
+++ src/lessons/Particles.jsx
@@ -10,7 +10,7 @@ import * as THREE from 'three';
 
 function Particles() {
   // for custom geometry particles
-  const particlesGeo = customParticleGeometry(500)
+  const particlesGeo = customParticleGeometry(5000, 10)
 
   return (
     <div style={{ height: '100vh', backgroundColor: 'rgb(26, 26, 26)' }}>
@@ -27,7 +27,7 @@ function Particles() {
 
         {/* PARTICLES */}
         <points>
-          <sphereBufferGeometry args={[1, 32, 32]} />
+          <sphereBufferGeometry args={[0.5, 32, 32]} />
           <pointsMaterial
             size={0.02}
             sizeAttenuation={true} // particle size is relative to distance from camera
@@ -36,7 +36,7 @@ function Particles() {
 
         {/* CUSTOM GEOMETRY PARTICLE */}
         <points geometry={particlesGeo} geometry-size={0.02}>
-          <pointsMaterial size={0.02} />
+          <pointsMaterial size={0.01} />
         </points>
       </Canvas>
     </div>
@@ -45,10 +45,10 @@ function Particles() {
 
 export default Particles;
 
-function customParticleGeometry(count) {
+function customParticleGeometry(count, spread) {
   const particlesGeometry = new THREE.BufferGeometry();
   let positions = new Float32Array(count * 3);
-  positions = positions.map((p) => (p = (Math.random() - 0.5) * 10));
+  positions = positions.map((p) => (p = (Math.random() - 0.5) * spread));
   particlesGeometry.setAttribute(
     'position',
     new THREE.BufferAttribute(positions, 3)
