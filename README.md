a50467f96553df05b14539a9eb2ffce04f975580
AliBakerSartawi <alisartawi@live.com>
2021-08-13 00:25:08 +0300

    duck loaded successfully

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 3js-journey
+
+## Loading Models With GLTFJSX
+  - A problem was encountered `Encountered two children with the same key`.
+  - The fix is to install the package globally &rarr; `npm i -g gltfjsx`
+  - Then provide the file and the directory
+
+    ```
+    $ npx gltfjsx public/duck/Duck.gltf
+    ```
--- src/lessons/ImportedModels.jsx
+++ src/lessons/ImportedModels.jsx
@@ -4,24 +4,38 @@ import { Physics, useBox, usePlane, useSphere } from '@react-three/cannon';
 import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
 import { useControls } from 'leva';
+import Duck from '../generatedModels/Duck';
+
+/**
+ * GLTF Models ⬇️
+ * https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0
+ * 
+ * GLTF Types ⬇️
+ * GLTF 
+ * GLTF-Binary 
+ * GLTF-Draco 
+ * GLTF-Embedded
+ * 
+ * Note: OS might hide GLTF file extensions, use code editor to verify 
+ * 
+ * GLTF Structure Example ⬇️
+ * Duck.gltf => JSON containing cameras, lights, materials & object transformation
+ * Duck0.bin => binary that usually contains geometries (vertices positions, UV coordinates, normal, colors, etc...)
+ * DuckCM.png => textures
+ * 
+ * GLTF-Binary Structure Example ⬇️
+ * Only one file / usually lighter / hard to alter its data
+ */
 
 /**
  * Plane
  */
 function Plane(props) {
-  const [plane] = usePlane(() => ({
-    mass: 0,
-    type: 'Static',
-    rotation: [-Math.PI / 2, 0, 0],
-    position: [0, 0, 0],
-    args: [25, 25],
-    ...props
-  }));
   return (
     <mesh
-      ref={plane}
+      // ref={plane}
       receiveShadow
-      // rotation-x={-Math.PI / 2}
+      rotation-x={-Math.PI / 2}
     >
       <planeBufferGeometry args={[25, 25]} />
       <meshStandardMaterial color={'grey'} />
@@ -30,83 +44,9 @@ function Plane(props) {
 }
 
 /**
- * Box
- */
-function Box({ color, x, y, z }) {
-  const [box] = useBox(() => ({
-    mass: 1,
-    position: [x, y, z],
-    rotation: [
-      (Math.random() * Math.PI) / 2,
-      (Math.random() * Math.PI) / 2,
-      (Math.random() * Math.PI) / 2
-    ],
-    args: [0.5, 0.5, 0.5]
-  }));
-  return (
-    <mesh
-      ref={box}
-      castShadow
-      // position={[0, 5, 0]}
-    >
-      <boxBufferGeometry args={[0.5, 0.5, 0.5]} />
-      <meshStandardMaterial color={color} />
-    </mesh>
-  );
-}
-
-function Boxes({boxes}) {
-  const rainingBoxes = useMemo(() => {
-    return (
-      <>
-        {new Array(boxes).fill(1).map((box, i) => {
-          const colors = ['lime', 'orange', 'royalblue', 'crimson'];
-          const colorIndex = i % 4;
-          const x = Math.random() < 0.5 ? 1 : -1;
-          const z = Math.random() < 0.5 ? 1 : -1;
-          return (
-            <Box
-              key={Math.random() * i}
-              x={x}
-              y={i + 3}
-              z={z}
-              color={colors[colorIndex]}
-            />
-          );
-        })}
-      </>
-    );
-  }, [boxes]);
-  return rainingBoxes;
-}
-
-/**
- * Sphere
- */
-function Sphere(props) {
-  const [sphere] = useSphere(() => ({
-    mass: 1,
-    args: [1],
-    position: [0, 0.75, 0]
-  }));
-  return (
-    <mesh castShadow ref={sphere}>
-      <sphereBufferGeometry args={[1]} />
-      <meshStandardMaterial color={'lightgrey'} />
-    </mesh>
-  );
-}
-
-/**
  * Main Component
  */
 function ImportedModels() {
-  // controls
-  const { gravity, boxes } = useControls({
-    gravity: { value: -9.82, min: -9.82, max: 0, step: 0.1 },
-    boxes: { value: 50, min: 1, max: 100, step: 1 },
-  });
-
   return (
     <div style={{ height: '100vh', backgroundColor: 'black' }}>
       <Canvas
@@ -121,12 +61,11 @@ function ImportedModels() {
         <axesHelper args={[10]} />
         <OrbitControls />
 
-        <Physics gravity={[0, gravity, 0]}>
-          <Plane />
-          {/* Raining Boxes */}
-          <Boxes boxes={boxes} />
-          <Sphere />
-        </Physics>
+        {/* OBJECTS */}
+        <Plane />
+
+        {/* MODELS */}
+        <Duck />
 
         <Lights />
       </Canvas>
