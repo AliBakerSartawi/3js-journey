84ac19e40e3bff3d901e8b5de2b1493d66db9760
AliBakerSartawi <alisartawi@live.com>
2021-08-15 01:47:07 +0300

    matrices explained

--- src/lessons/shaders/plane/vertex.vs.glsl
+++ src/lessons/shaders/plane/vertex.vs.glsl
@@ -1,9 +1,30 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
 
-// retreiving position value from geometry
-// the same position attribute provided in the buffer
+/* 
+ * each matrix will transform the position until we get
+ * the final clip space coordinates
+ * 
+ * they are uniforms because they are the same for all vertices
+ * each matrix does a part in the transformation
+ * to apply a matrix, we multiply it
+ * must have the same size as coordinates (mat4 for vec4)
+ *
+ * modelMatrix       => apply transformations relative to the mesh
+ *                   => (position, rotation, scale)
+ *
+ * viewMatrix        => apply transformations relative to the camera
+ *                   => (position, rotation, field of view, near, far)
+ *
+ * projectionMatrix  => transform the coordinates into the clip space coordinates
+ */
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+/*
+ * retreiving position value from geometry
+ * the same position attribute provided in the float array
+ * geo.setAttribute('position', positionsAttribute)
+ */
 attribute vec3 position;
 
 // main is called automatically, and is void
