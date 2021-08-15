84e3aaa4f0a1306d3f2b1920f0f94e09c88f0e74
AliBakerSartawi <alisartawi@live.com>
2021-08-16 02:29:00 +0300

    shaderPatterns cleanup

--- src/lessons/ShaderPatterns.jsx
+++ src/lessons/ShaderPatterns.jsx
@@ -10,138 +10,16 @@ import { folder, Leva, useControls } from 'leva';
 import vertexShader from '!!raw-loader!./shaders/shaderPatterns/vertex.vs.glsl';
 import fragmentShader from '!!raw-loader!./shaders/shaderPatterns/fragment.fs.glsl';
 
-// glslify seems to import them normally without raw-loader, but I get a strange missing semicolon error
 // import vertexShader from './shaders/shaderPatterns/vertex.vs.glsl';
 // import fragmentShader from './shaders/shaderPatterns/fragment.fs.glsl';
 
 // glsl import
 // import glsl from 'babel-plugin-glsl/macro';
 
-/**
- * Problem importing GLSL files:
- *
- * the problem is that it is imported as a static file (like images)
- * but we need it as a string, webpack must be configured, which isn't an easy task in a react app
- *
- * the following must be added to /bundler/webpack.common.js, in the rules array
- *
- * {
- *    test: /\.(vs.glsl|fs.glsl|or any other extensions)$/,
- *    exclude: /node_modules/,
- *    use: {
- *        'raw-loader'
- *    }
- * }
- *
- * 🟢 Solution: use raw-loader package, insert !!raw-loader! before file path to override webpack configs
- *
- */
-
-/**
- * GLSL Rules:
- *
- * - close to C language
- * - you can't log anything
- * - semicolons are a must
- * - variables are typed
- *
- * - Rules:
- *    - 🟢 float foo = 0.1212;
- *    - 🟢 float bar = 1.0; => must always have decimals
- *    - 🟢 float duh = foo * bar; // must always have decimals
- *    - 🔴 float salamander = 1;
- *    - 🟢 int cheese = 1;
- *    - 🟢 int mango = - 12;
- *    - 🟢 int yuck = cheese / mango; // but if you get a float, you're in trouble
- *    - 🔴 int salsa = 1.0;
- *    - 🔴 float mix = bar * cheese; // cannot mix int with float
- *    - 🟢 float convert = bar * float(cheese); // but can convert on the fly
- *    - 🟢 bool convinceMe = true;
- *    - 🟢 bool notConvinced = false;
- *
- *    - vec2 (to store 2 coordinates(x, y))
- *    - 🟢 vec2 sup = vec2(1.0, 2.0);
- *    - 🔴 vec2 emptyInside = vec2(); // will cause an error, unlike THREE
- *    - 🟢 vec2 mono = vec2(1.0); // will use value for x & y
- *    - 🟢 vec2 mutable = vec2(1.0); mutable.x = 2.0; mutable.y = 3.0;
- *    - 🟢 mutable *= 2.0; // will affect both x & y
- *
- *    - vec3 (to store 3 coordinates(x, y, z))
- *    - 🟢 vec3 hey = vec3(1.0);
- *    - 🟢 vec3 yo = vec3(1.0, 3.6, 4.5);
- *    - 🟢 yo.z = 9.9;
- *    - 🟢 vec3 fromVec2 = vec3(sup, 4.5);
- *    - or the opposite (called swizzle)
- *    - 🟢 vec2 fromVec3 = hey.xy; // even xz or yx, but not xg or by
- *
- *    - instead of (x, y, z), you can use (r, g, b) aliases
- *    - or even use them interchangeably on the same variable
- *    - 🟢 yo.x = 0.7;
- *    - 🟢 yo.g = 0.9;
- *    - 🟢 yo.b = 0.3;
- *
- *    - vec4 (to store 4 coordinates(x, y, z, w)) || (r, g, b, a)
- *    - 🟢 vec4 comeOn = vec4(1.0, 2.0, 3.0, 4.0);
- *    - 🟢 vec4 really = vec4(comeOn.zw, vec2(5.0, 6.0));
- *    - 🟢 float backAgain = really.a;
- *
- *    - functions are typed as well
- *    - float sum(float a, float b) { return a + b; };
- *
- *    - classic built-in functions
- *      - sin, cos, max, min, pow, exp, mod, clamp
- *
- *    - practical built-in functions
- *      - cross, dot, mix, step, smoothstep, length, distance, reflect, refract, normalize
- *
- * - Documentation: (not beginner-friendly)
- *   - https://www.shaderific.com/glsl-functions
- *   - https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/indexflat.php
- *   - https://thebookofshaders.com/glossary/
- *
- * - Inspirational Links:
- *   - https://thebookofshaders.com/
- *   - https://www.shadertoy.com/
- *   - https://www.youtube.com/channel/UCcAlTqd9zID6aNX3TzwxJXg
- *   - https://www.youtube.com/channel/UC8Wzk_R1GoPkPqLo-obU_kQ
- */
-
-/**
- * Shaders
- * Written in GLSL
- *
- * GLSL => stands for openGL (Graphics Library) Shading Language
- *      => positions each vertex of a geometry
- *      => colorizes each visible pixel (fragment) of that geometry
- *      => fragments are to the renderer like pixels are to a screen
- *      => data (vertices, meshes, lights, camera, etc.) are sent to the GPU, which process them according to shader instructions
- *
- *      => Types:
- *         => Vertex Shader   => the same vertex shader code will position all vertices (runs for each vertex)
- *                            => data that differ from vertex to vertex (like vertex position & vertexColor) are called: 🟠 attributes
- *                            => data that are similar to all vertices (like mesh position & camera) are called: 🔵 uniforms
- *         => Fragment Shader => once vertices are placed, GPU knows the visible fragments and proceeds to the fragment shader
- *                            => fragment shaders only have uniforms, no attributes, but...
- *                            => data can be sent from the vertex shader, they're called: 🟣 varyings
- *                            => 🟣 varyings values are interpolated between the vertices
- *                               => for example: if each vertex has a color, the fragment will have a percentage of that color
- *                                  depending on how close it is to that vertex
- *
- *      => Why Custom Shaders => very performant
- *                            => custom post-processing (effects)
- *                            => can do what built in materials can't
- *
- *      => ShaderMaterial will have some code added to the shader
- *      => RawShaderMaterial is... raw 🤓
- *
- *      => glsl linting => https://www.youtube.com/watch?v=NQ-g6v7GtoI
- */
-
 function BufferAttributes() {
   const buffer = useRef();
 
   const [array, bufferCount, itemSize] = useMemo(() => {
-    // if (count) {
     const bufferCount = 1089;
     const itemSize = 1;
     const array = new Float32Array(bufferCount);
@@ -150,15 +28,11 @@ function BufferAttributes() {
       array[i] = Math.random();
     }
     return [array, bufferCount, itemSize];
-    // } else {
-    //   return [[], 0, 0];
-    // }
   }, []);
 
   return (
     <bufferAttribute
       ref={buffer}
-      // Bruno Simon naming convention => aRandom, a for attribute
       attachObject={['attributes', 'aRandom']}
       count={bufferCount}
       array={array}
@@ -171,9 +45,7 @@ const PlaneShaderMaterial = shaderMaterial(
   {
     uTime: 0,
     uFrequency: new THREE.Vector2(10, 5),
-    // even if this is orange, the color will be taken from props
-    // or simply new THREE.Color(0,0,0)
-    uColor: new THREE.Color('orange'),
+    uColor: new THREE.Color(0, 0, 0),
     uAlpha: 0.5,
     uTexture: new THREE.Texture(),
     uNormals: false,
@@ -220,11 +92,6 @@ function Plane() {
 
   useFrame(({ clock }) => (shaderMaterial.current.uTime = clock.elapsedTime));
 
-  useEffect(() => {
-    // plane.current.geometry.attributes.position.count => exact number of vertices
-    // plane.current && console.log(plane.current.geometry.attributes);
-  }, [plane]);
-
   return (
     <mesh ref={plane} transparent={transparent} scale-y={2 / 3}>
       <planeBufferGeometry args={[1, 1, 32, 32]}>
@@ -235,32 +102,15 @@ function Plane() {
         wireframe={wireframe}
         transparent={transparent}
         side={THREE.DoubleSide}
-        // uniforms
+        // uniforms 👇
         uFrequency={new THREE.Vector2(uFrequencyX, uFrequencyY)}
         // uTime is provided by altering the ref directly inside useFrame
-
-        // providing (color) in uniform might miss with color attribute
         uColor={new THREE.Color(color)}
         uAlpha={opacity}
-        // textures => resolution should be in power of two (mip-mapping)
         uTexture={image}
         uNormals={normals}
         uNormals2={normals2}
       />
-
-      {/* this is the old method, not great for re-renders */}
-      {/* <rawShaderMaterial
-          // putting the shaders in args prop as object causes error (argsNew.some is not a function)
-          vertexShader={vertexShader}
-          fragmentShader={fragmentShader}
-          // wireframe
-          side={THREE.DoubleSide}
-          transparent={transparent}
-          uniforms={{
-            uFrequency: { value: new THREE.Vector2(uFrequencyX, uFrequencyY) },
-            uTime: { value: 0}
-          }}
-        /> */}
     </mesh>
   );
 }
