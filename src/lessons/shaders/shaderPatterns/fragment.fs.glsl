83ff0e51999aa392d18451e110de13ed2f0bd618
AliBakerSartawi <alisartawi@live.com>
2021-08-16 18:59:02 +0300

    perlin noise

--- src/lessons/shaders/shaderPatterns/fragment.fs.glsl
+++ src/lessons/shaders/shaderPatterns/fragment.fs.glsl
@@ -26,6 +26,52 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid) {
   );
 }
 
+// most popular perlin noises 👇
+// https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+
+// permute function is manually provided from another algo in perlin noises gist
+vec4 permute(vec4 x) {
+  return mod(((x*34.0)+1.0)*x, 289.0);
+}
+
+//	Classic Perlin 2D Noise 
+//	by Stefan Gustavson
+//
+vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+float cnoise(vec2 P){
+  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+  Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+  vec4 ix = Pi.xzxz;
+  vec4 iy = Pi.yyww;
+  vec4 fx = Pf.xzxz;
+  vec4 fy = Pf.yyww;
+  vec4 i = permute(permute(ix) + iy);
+  vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+  vec4 gy = abs(gx) - 0.5;
+  vec4 tx = floor(gx + 0.5);
+  gx = gx - tx;
+  vec2 g00 = vec2(gx.x,gy.x);
+  vec2 g10 = vec2(gx.y,gy.y);
+  vec2 g01 = vec2(gx.z,gy.z);
+  vec2 g11 = vec2(gx.w,gy.w);
+  vec4 norm = 1.79284291400159 - 0.85373472095314 * 
+    vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+  g00 *= norm.x;
+  g01 *= norm.y;
+  g10 *= norm.z;
+  g11 *= norm.w;
+  float n00 = dot(g00, vec2(fx.x, fy.x));
+  float n10 = dot(g10, vec2(fx.y, fy.y));
+  float n01 = dot(g01, vec2(fx.z, fy.z));
+  float n11 = dot(g11, vec2(fx.w, fy.w));
+  vec2 fade_xy = fade(Pf.xy);
+  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+  return 2.3 * n_xy;
+}
+
 void main() {
 
   // // pattern #1 (like normals)
@@ -227,9 +273,71 @@ void main() {
   // );
   // strength = 1.0 - step(0.01, abs(distance(wavedUV, vec2(0.5)) - 0.25));
 
-  // pattern #40 (angles)
-  strength = vUV.x;
+  // // pattern #40 (angles)
+  // float angle = atan(vUV.x, vUV.y);
+  // strength = angle;
 
+  // // pattern #41 (angle starting from center (half-square))
+  // float angle = atan(vUV.x - 0.5, vUV.y - 0.5); 
+  // strength = angle;
 
-  gl_FragColor = vec4(vec3(strength), uAlpha);
+  // // pattern #42 (circular angle starting from center)
+  // float angle = atan(vUV.x - 0.5, vUV.y - 0.5); 
+  // angle /= PI * 2.0;
+  // angle += 0.5;
+  // strength = angle;
+
+  // // pattern #43 (circular angle starting from center * 20 steps)
+  // float angle = atan(vUV.x - 0.5, vUV.y - 0.5); 
+  // angle /= PI * 2.0;
+  // angle += 0.5;
+  // angle *= 20.0; // crack to 100.0 for illusion pattern 😵
+  // angle = mod(angle, 1.0);
+  // strength = angle;
+
+  // // pattern #44 (circular angle starting from center steps with gradient)
+  // float angle = atan(vUV.x - 0.5, vUV.y - 0.5); 
+  // angle /= PI * 2.0;
+  // angle += 0.5;
+  // strength = sin(angle * 100.0);
+
+  // // pattern #45 (sinusoid - some shape in the middle)
+  // float angle = atan(vUV.x - 0.5, vUV.y - 0.5); 
+  // angle /= PI * 2.0;
+  // angle += 0.5;
+  // float sinusoid = sin(angle * 100.0);
+  // float radius = 0.25 + sinusoid * 0.02;
+  // strength = 1.0 - step(0.01, abs(distance(vUV, vec2(0.5)) - radius));
+
+  // // pattern #46 (perlin noise (smoke, cloud, water, terrain, or even to animate grass in the wind etc...) bsically a nature algorithm)
+  // // there are many perlin noise algorithms (2D, 3D, 4D)
+  // // most popular perlin noises 👇
+  // // https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+  // strength = cnoise(vUV * 10.0);
+
+  // // pattern #47 (perlin noise sharp)
+  // // step is 0.0, because perlin noise goes from -something to +something
+  // strength = step(0.0, cnoise(vUV * 10.0));
+
+  // // pattern #48 (perlin noise neon outlined)
+  // strength = 1.0 - abs(cnoise(vUV * 10.0));
+
+  // // pattern #49 (perlin noise mary-jane-high pattern)
+  // // strength = step(0.9, 1.0 - abs(cnoise(vUV * 50.0))); // wrong answer, but nice pattern though
+  // strength = sin(cnoise(vUV * 10.0) * 20.0); // 😲 AMAZING PATTERN
+
+  // pattern #50 (perlin noise mary-jane-high pattern sharp)
+  strength = step(0.9, sin(cnoise(vUV * 10.0) * 20.0)); // 😲 WWOOOOWWWW
+
+  // MIXING COLORS
+  vec3 blackColor = vec3(0.0);
+  vec3 uvColor = vec3(vUV, 0.5);
+
+  // strength will be an interpolation between black and uvColor
+  vec3 mixedColor = mix(blackColor, uvColor, strength);
+
+  gl_FragColor = vec4(mixedColor, uAlpha);
+
+  // // Black and White version
+  // gl_FragColor = vec4(vec3(strength), uAlpha);
 }
