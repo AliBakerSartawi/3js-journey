9b7dd0972310fdee8175a53f431181e169ea1941
AliBakerSartawi <alisartawi@live.com>
2021-08-16 17:44:43 +0300

    sin cos shenanigans

--- src/lessons/shaders/shaderPatterns/fragment.fs.glsl
+++ src/lessons/shaders/shaderPatterns/fragment.fs.glsl
@@ -1,5 +1,8 @@
 precision mediump float;
 
+// constant, without semicolon or equal sign
+#define PI 3.1415926535897932384626433832795
+
 uniform float uAlpha;
 
 // 0.0, 0.0 bottom-left => 1.0, 1.0 top-right
@@ -11,6 +14,16 @@ varying vec2 vUV;
 // this function is psuedo-random, not true random
 float random(vec2 st) {
   return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453132);
+}
+
+/*
+ * mid => pivot point
+ */
+vec2 rotate(vec2 uv, float rotation, vec2 mid) {
+  return vec2(
+    cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+    cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+  );
 }
 
 void main() {
@@ -154,9 +167,68 @@ void main() {
   // // point light effect / faraway star
   // strength = 0.015 / distance(vUV, vec2(0.5, 0.5)) - 0.1;
   
-  // pattern #30 (like previous but stretched on x)
-  // point light effect / faraway star
-  strength = 0.015 / distance(vUV, vec2(0.5, 0.5)) - 0.1;
+  // // pattern #30 (like previous but stretched on x)
+  // // point light effect / faraway star
+  // vec2 lightUV = vec2(
+  //   vUV.x * 0.25 + 0.375,
+  //   vUV.y
+  // );
+  // strength = 0.015 / distance(lightUV, vec2(0.5, 0.5)) - 0.1;
+  
+  // // pattern #31 (starry shape in the middle)
+  // vec2 lightUVX = vec2(vUV.x * 0.25 + 0.375, vUV.y);
+  // float lightX = 0.015 / distance(lightUVX, vec2(0.5, 0.5)) - 0.1;
+  // vec2 lightUVY = vec2(vUV.y * 0.25 + 0.375, vUV.x);
+  // float lightY = 0.015 / distance(lightUVY, vec2(0.5, 0.5)) - 0.1;
+  // strength = lightX + lightY;
+
+  // // pattern #32 (like previous but rotated PI / 4)
+  // float pi = 3.14;
+  // // or
+  // pi = 3.1415926535897932384626433832795;
+  // // or define it as constant at the begging of the file
+  // vec2 rotatedUV = rotate(vUV, PI / 4.0, vec2(0.5));
+  // vec2 lightUVX = vec2(rotatedUV.x * 0.25 + 0.375, rotatedUV.y);
+  // float lightX = 0.015 / distance(lightUVX, vec2(0.5, 0.5)) - 0.1;
+  // vec2 lightUVY = vec2(rotatedUV.y * 0.25 + 0.375, rotatedUV.x);
+  // float lightY = 0.015 / distance(lightUVY, vec2(0.5, 0.5)) - 0.1;
+  // strength = lightX + lightY;
+
+  // // pattern #33 (black solid circle in the middle)
+  // strength = step(0.25, distance(vUV, vec2(0.5)));
+
+  // // pattern #34 (black eclipse outline circle in the middle)
+  // strength = abs(distance(vUV, vec2(0.5)) - 0.25);
+
+  // // pattern #35 (black solid outline circle in the middle)
+  // strength = step(0.01, abs(distance(vUV, vec2(0.5)) - 0.25));
+
+  // // pattern #36 (white solid outline circle in the middle)
+  // strength = 1.0 - step(0.01, abs(distance(vUV, vec2(0.5)) - 0.25));
+
+  // // pattern #37 (white solid outline circle in the middle DISTORTED)
+  // vec2 wavedUV = vec2(
+  //   vUV.x, 
+  //   vUV.y + sin(vUV.x * 30.0) * 0.1
+  // );
+  // strength = 1.0 - step(0.01, abs(distance(wavedUV, vec2(0.5)) - 0.25));
+
+  // // pattern #38 (weird shapes)
+  // vec2 wavedUV = vec2(
+  //   vUV.x + sin(vUV.y * 30.0) * 0.1, 
+  //   vUV.y + sin(vUV.x * 30.0) * 0.1
+  // );
+  // strength = 1.0 - step(0.01, abs(distance(wavedUV, vec2(0.5)) - 0.25));
+
+  // // pattern #39 (super weird shapes)
+  // vec2 wavedUV = vec2(
+  //   vUV.x + sin(vUV.y * 100.0) * 0.1, 
+  //   vUV.y + sin(vUV.x * 100.0) * 0.1
+  // );
+  // strength = 1.0 - step(0.01, abs(distance(wavedUV, vec2(0.5)) - 0.25));
+
+  // pattern #40 (angles)
+  strength = vUV.x;
 
 
   gl_FragColor = vec4(vec3(strength), uAlpha);
